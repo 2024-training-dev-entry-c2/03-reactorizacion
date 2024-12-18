397bf04bc85d0cb92039c361f3e73331e1102507
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2024-12-18 18:27:46 -0500

    feat(classes): update Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
+
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String nationality;
+    private String phone;
+    private LocalDate birthDate;
+
+    public Customer(String firstName, String lastName, String email, String nationality, String phone, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.nationality = nationality;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+
+    public Boolean validateCustomer(String email, LocalDate birthDate){
+        return email.equals(this.email) && birthDate.equals(this.birthDate);
+    }
 }
