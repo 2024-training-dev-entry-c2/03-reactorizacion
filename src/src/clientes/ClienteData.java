3fe0b69012e81633a191ad48506ac194a8aab8f6
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2024-12-18 18:09:49 -0500

    feat(cliente): se agrego la clase Cliente

--- src/src/clientes/ClienteData.java
+++ src/src/clientes/ClienteData.java
@@ -5,6 +5,65 @@ public class ClienteData {
     String nombre;
     String apellido;
     String fechaNacimiento;
-    
+    String numeroTel;
+    String correo;
+    String nacinalidad;
 
+
+    public ClienteData(String nombre, String apellido, String fechaNacimiento, String numeroTel, String correo, String nacinalidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.numeroTel = numeroTel;
+        this.correo = correo;
+        this.nacinalidad = nacinalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNumeroTel() {
+        return numeroTel;
+    }
+
+    public void setNumeroTel(String numeroTel) {
+        this.numeroTel = numeroTel;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNacinalidad() {
+        return nacinalidad;
+    }
+
+    public void setNacinalidad(String nacinalidad) {
+        this.nacinalidad = nacinalidad;
+    }
 }
