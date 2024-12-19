be79b3f39266567233cd99aef252745f901d5684
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2024-12-18 20:13:10 -0400

    init: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Manuel Aguilera / @aguileradev
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello");
+
+    }
+}
