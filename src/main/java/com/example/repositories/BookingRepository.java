bf706dd2f5de591d4f93b7f3de117cf66d16a264
Johan <johanvilla64@gmail.com>
2024-12-22 12:41:58 -0500

    feat: :sparkles: add BookingRepository class for saving bookings and add new method in accommodation repo for filtering by accomodation name

--- src/main/java/com/example/repositories/BookingRepository.java
+++ src/main/java/com/example/repositories/BookingRepository.java
@@ -1,4 +1,52 @@
 package com.example.repositories;
 
+import com.example.models.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookingRepository {
+  private static BookingRepository instance;
+  private List<Booking> bookings;
+
+  private BookingRepository() {
+    initialize();
+  }
+
+  public static BookingRepository getInstance() {
+    if (instance == null) {
+      instance = new BookingRepository();
+    }
+    return instance;
+  }
+
+  private void initialize() {
+    bookings = new ArrayList<>();
+  }
+
+  public List<Booking> getBookings() {
+    return bookings;
+  }
+
+  public void setBookings(List<Booking> bookings) {
+    this.bookings = bookings;
+  }
+
+  public void addBooking(Booking booking) {
+    bookings.add(booking);
+  }
+
+  public void removeBooking(Booking booking) {
+    bookings.remove(booking);
+  }
+
+  public List<Booking> getBookingsByAccommodation(String accommodaation) {
+    List<Booking> bookingsByAccommodation = new ArrayList<>();
+    for (Booking booking : bookings) {
+      if (booking.getAccomodation().getName().equalsIgnoreCase(accommodaation)) {
+        bookingsByAccommodation.add(booking);
+      }
+    }
+    return bookingsByAccommodation;
+  }
 }
