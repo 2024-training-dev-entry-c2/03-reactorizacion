5147ec997d0300b70e68d1b6a3c1a59d70cacb8d
Johan <johanvilla64@gmail.com>
2024-12-22 10:35:58 -0500

    feat: :construction: working on consoleUtils (input)

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,20 +3,21 @@ package com.example;
 import com.example.repositories.AccommodationRepository;
 import com.example.services.BookingService;
 import com.example.services.MenuService;
-import com.example.services.InputInputValidatorService;
+import com.example.services.booking.MakeBooking;
 import com.example.services.interfaces.IMenuService;
-import com.example.services.interfaces.IInputValidatorService;
+import com.example.utils.ConsoleUtils;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         AccommodationRepository accommodationRepository = AccommodationRepository.getInstance();
-        Scanner scanner = new Scanner(System.in);
-        IInputValidatorService validatorService = new InputInputValidatorService(scanner);
-        IMenuService menuService = new MenuService(validatorService);
-        BookingService bookingService = new BookingService(accommodationRepository, menuService, validatorService);
+        ConsoleUtils validatorService = new ConsoleUtils();
+//        IMenuService menuService = new MenuService(validatorService);
+//        BookingService bookingService = new BookingService(accommodationRepository, menuService, validatorService);
 
-        bookingService.start();
+//        bookingService.start();
+        MakeBooking makeBooking = new MakeBooking(accommodationRepository, validatorService);
+        makeBooking.execute();
     }
 }
---
+++ src/main/java/com/example/utils/ConsoleUtils.java
@@ -0,0 +1,60 @@
+package com.example.utils;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class ConsoleUtils {
+  private final Scanner scanner = new Scanner(System.in);
+
+  public String getString(String message) {
+    System.out.println(message);
+    return scanner.next();
+  }
+
+  public Integer getInteger(String message) {
+    try {
+      System.out.println(message);
+      int value = scanner.nextInt();
+
+      if (value < 0) {
+        System.out.println("Ingresa un número positivo.");
+        return getInteger(message);
+      }
+
+      return value;
+    } catch (Exception e) {
+      System.err.println("Ingresa un número con formato valido.");
+      return getInteger(message);
+    }
+  }
+
+  public LocalDate getLocalDate(String message) {
+    try {
+      System.out.println(message);
+      String input = scanner.nextLine();
+      return LocalDate.parse(input);
+
+    } catch (NullPointerException e) {
+      System.err.println("Entrada no puede ser null.");
+      return getLocalDate(message);
+
+    } catch (Exception e) {
+      System.err.println("Ingresa una fecha con formato valido (yyyy-mm-dd).");
+      return getLocalDate(message);
+    }
+  }
+
+  public LocalTime getLocalTime(String message) {
+    try {
+      System.out.println(message);
+      String input = scanner.nextLine();
+      LocalTime value = LocalTime.parse(input);
+      return value;
+
+    } catch (Exception e) {
+      System.err.println("Ingresa una hora con formato valido (HH:mm).");
+      return getLocalTime(message);
+    }
+  }
+}
