b943d177a4b82f3d283faa189243c2ff6496c9f6
Johan <johanvilla64@gmail.com>
2024-12-22 22:09:59 -0500

    feat: :sparkles: add new service to show saved bookings by accommodation

---
+++ src/main/java/com/example/services/booking/ShowBookings.java
@@ -0,0 +1,35 @@
+package com.example.services.booking;
+
+import com.example.repositories.BookingRepository;
+import com.example.services.interfaces.ICommand;
+import com.example.utils.ConsoleUtils;
+
+public class ShowBookings implements ICommand<String> {
+  private final BookingRepository repository;
+  private final ConsoleUtils console;
+
+  public ShowBookings(BookingRepository repository, ConsoleUtils console) {
+    this.repository = repository;
+    this.console = console;
+  }
+
+  public BookingRepository getRepository() {
+    return repository;
+  }
+
+  public ConsoleUtils getConsole() {
+    return console;
+  }
+
+  @Override
+  public String execute() {
+    String accommodationName = console.getString("Ingrese el nombre del alojamiento para ver sus reservas: ");
+    repository.getBookings().forEach((booking) -> {
+      if (booking.getAccomodation().getName().equalsIgnoreCase(accommodationName)) {
+        System.out.println(booking.toString());
+      }
+    });
+    return "Reservas del alojamiento " + accommodationName;
+  }
+
+}
