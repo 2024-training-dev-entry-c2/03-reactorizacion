162308a412bf20a8e6f3c62cf461b91944fbeccb
Johan <johanvilla64@gmail.com>
2024-12-23 10:07:30 -0500

    refactor: :reclycle: implemented new makeBooking class and IBookingService updated with just one method

--- src/main/java/com/example/services/BookingService.java
+++ src/main/java/com/example/services/BookingService.java
@@ -2,6 +2,8 @@ package com.example.services;
 
 import com.example.repositories.AccommodationRepository;
 import com.example.models.Stay;
+import com.example.repositories.BookingRepository;
+import com.example.services.booking.MakeBooking;
 import com.example.services.interfaces.IBookingService;
 import com.example.services.interfaces.IMenuService;
 import com.example.utils.ConsoleUtils;
@@ -9,80 +11,32 @@ import com.example.utils.ConsoleUtils;
 public class BookingService implements IBookingService {
 
     private final AccommodationRepository accommodationRepository;
+    private final BookingRepository bookingRepository;
     private final IMenuService menuService;
     private final ConsoleUtils validatorService;
 
-    public BookingService(AccommodationRepository accommodationRepository, IMenuService menuService, ConsoleUtils validatorService) {
+    public BookingService(AccommodationRepository accommodationRepository, BookingRepository bookingRepository, IMenuService menuService, ConsoleUtils validatorService) {
         this.accommodationRepository = accommodationRepository;
+        this.bookingRepository = bookingRepository;
         this.menuService = menuService;
         this.validatorService = validatorService;
     }
 
+    @Override
     public void start() {
         Integer option = menuService.mainMenu();
-        String selectedCity;
-
         do {
             switch (option) {
                 case 1 -> {
                     System.out.println("Reservar alojamiento");
-                    askForCity();
-                    askForType();
+                    MakeBooking makeBooking = new MakeBooking(accommodationRepository, bookingRepository, validatorService);
+                    makeBooking.execute();
                 }
                 case 2 -> System.out.println("Ver reservas");
                 case 3 -> System.out.println("Modificar reserva");
             }
             option = menuService.mainMenu();
         } while (option != 4);
-    }
-
-    public void askForCity() {
-        listCities();
-        String selectedCity = validatorService.getString("Por favor, seleccione una de las ciudades:");
-        if (!validateCity(selectedCity)) {
-            System.out.println("La ciudad seleccionada no es válida. Intente de nuevo.");
-            return;
-        }
-    }
-
-    public void askForType() {
-//        String this.selectedCity;
-        String selectedType = validatorService.getString("¿Qué tipo de reserva desea hacer? (1.Alojamiento/ 2.Día de sol)");
-//        listStaysByCity(selectedCity);
-        String selectedAccommodation = validatorService.getString("Ingrese el nombre del alojamiento:");
-
-        int numberOfAdults = validatorService.getInteger("Ingrese cantidad de adultos que reservarán:");
-        int numberOfChildren = validatorService.getInteger("Ingrese cantidad de niños que reservarán:");
-        String confirmation = validatorService.getString("¿Desea confirmar la reserva? (Sí/No)");
-
-        if (confirmation.equalsIgnoreCase("Si")) {
-            System.out.println("Reserva realizada con éxito para " + selectedAccommodation + " en " + "ciudad" + ".");
-        } else {
-            System.out.println("Reserva cancelada.");
-        }
-    }
-
-    public void listCities() {
-        for (String city : accommodationRepository.getCities()) {
-            System.out.print(city + " | ");
-        }
-        System.out.println();
-    }
-
-    public void listStaysByCity(String city) {
-        System.out.println("Alojamientos disponibles en " + city + ":");
-        for (Stay stay : accommodationRepository.getStays()) {
-            if (stay.getCity().equalsIgnoreCase(city)) {
-                System.out.println("  * " + stay.getName() + " - desde $" + stay.getBasePrice() + " USD por noche");
-            }
-        }
-    }
-
-    public void listStayByTye(String type, String city) {
-        System.out.println("Alojamientos disponibles en " + city + ":");
-        for (Stay stay : accommodationRepository.getStays()) {
-            stay.describe(); //TODO:Metodo descriptivo
-        }
     }
 
     private boolean validateCity(String city) {
--- src/main/java/com/example/services/interfaces/IBookingService.java
+++ src/main/java/com/example/services/interfaces/IBookingService.java
@@ -3,6 +3,5 @@ package com.example.services.interfaces;
 public interface IBookingService {
 
     void start();
-    void askForCity();
-    void askForType();
+
 }
