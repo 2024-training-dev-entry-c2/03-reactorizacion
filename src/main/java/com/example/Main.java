dde89d25328d05537f828fe2210d87aaf56630d8
Johan <johanvilla64@gmail.com>
2024-12-23 10:08:36 -0500

    feat: :construction: WIP main updated with MakeBooking logic flow

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,23 +1,21 @@
 package com.example;
 
 import com.example.repositories.AccommodationRepository;
+import com.example.repositories.BookingRepository;
 import com.example.services.BookingService;
 import com.example.services.MenuService;
-import com.example.services.booking.MakeBooking;
 import com.example.services.interfaces.IMenuService;
 import com.example.utils.ConsoleUtils;
 
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        AccommodationRepository accommodationRepository = AccommodationRepository.getInstance();
-        ConsoleUtils validatorService = new ConsoleUtils();
-//        IMenuService menuService = new MenuService(validatorService);
-//        BookingService bookingService = new BookingService(accommodationRepository, menuService, validatorService);
+  public static void main(String[] args) {
+    AccommodationRepository accommodationRepository = AccommodationRepository.getInstance();
+    BookingRepository bookingRepository = BookingRepository.getInstance();
+    ConsoleUtils validatorService = new ConsoleUtils();
+    IMenuService menuService = new MenuService(validatorService);
+    BookingService bookingService = new BookingService(accommodationRepository, bookingRepository, menuService, validatorService);
 
-//        bookingService.start();
-        MakeBooking makeBooking = new MakeBooking(accommodationRepository, validatorService);
-        makeBooking.execute();
-    }
+    bookingService.start();
+//    menuService.mainMenu();
+  }
 }
